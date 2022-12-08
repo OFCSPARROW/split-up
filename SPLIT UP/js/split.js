687f18a53e68fdf7aa8722b342a40200b903bada
OFC SPARROW <110273928+OFCSPARROW@users.noreply.github.com>
2022-12-08 12:43:28 +0530

    split up

---
+++ SPLIT UP/js/split.js
@@ -0,0 +1,21 @@
+var a=2003;
+var b;
+var c;
+var d;
+var e=10;
+
+( Math.floor( b= a / e ));
+( Math.floor( c= b / e ));
+( Math.floor( d= c / e ));
+
+
+ console.log( Math.floor( a % e ));
+ console.log( Math.floor( b % e ));
+ console.log( Math.floor( c % e ));
+ console.log( Math.floor( d % e ));
+ 
+
+
+
+
+
